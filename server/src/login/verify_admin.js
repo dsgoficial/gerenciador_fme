33fde5cb4896b323a0f425c6171af341f048c151
Felipe Diniz <diniz.ime@gmail.com>
2020-08-30 17:10:33 -0300

    fix check

--- server/src/login/verify_admin.js
+++ server/src/login/verify_admin.js
@@ -1,33 +1,34 @@
-'use strict'
+"use strict";
 
-const { AppError, asyncHandler, httpCode } = require('../utils')
+const { AppError, asyncHandler, httpCode } = require("../utils");
 
-const { db } = require('../database')
+const { db } = require("../database");
 
-const validateToken = require('./validate_token')
+const validateToken = require("./validate_token");
 
 // middleware para verificar se o usuário é administrador
 const verifyAdmin = asyncHandler(async (req, res, next) => {
-  const token = req.headers.authorization
+  const token = req.headers.authorization;
 
-  const decoded = await validateToken(token)
+  const decoded = await validateToken(token);
 
   if (!decoded.uuid) {
-    throw new AppError('Falta informação de usuário')
+    throw new AppError("Falta informação de usuário");
   }
   const response = await db.conn.oneOrNone(
-    'SELECT administrador FROM dgeo.usuario WHERE uuid = $<usuarioUuid> and ativo IS TRUE',
+    "SELECT administrador FROM dgeo.usuario WHERE uuid = $<usuarioUuid> and ativo IS TRUE",
     { usuarioUuid: decoded.uuid }
-  )
+  );
   if (!response.administrador) {
     throw new AppError(
-      'Usuário necessita ser um administrador',
+      "Usuário necessita ser um administrador",
       httpCode.Forbidden
-    )
+    );
   }
-  req.usuarioUuid = decoded.uuid
+  req.usuarioUuid = decoded.uuid;
+  req.administrador = true;
 
-  next()
-})
+  next();
+});
 
-module.exports = verifyAdmin
+module.exports = verifyAdmin;
--- server/src/login/verify_login.js
+++ server/src/login/verify_login.js
@@ -1,25 +1,35 @@
-'use strict'
+"use strict";
 
-const { AppError, asyncHandler, httpCode } = require('../utils')
+const { AppError, asyncHandler, httpCode } = require("../utils");
 
-const validateToken = require('./validate_token')
+const { db } = require("../database");
+
+const validateToken = require("./validate_token");
 
 // middleware para verificar o JWT
 const verifyLogin = asyncHandler(async (req, res, next) => {
   // verifica o header authorization para pegar o token
-  const token = req.headers.authorization
+  const token = req.headers.authorization;
 
-  const decoded = await validateToken(token)
+  const decoded = await validateToken(token);
 
   if (req.params.usuario_uuid && decoded.uuid !== req.params.usuario_uuid) {
     throw new AppError(
-      'Usuário só pode acessar sua própria informação',
+      "Usuário só pode acessar sua própria informação",
       httpCode.Unauthorized
-    )
+    );
   }
 
-  req.usuarioUuid = decoded.uuid
-  next()
-})
+  const response = await db.conn.oneOrNone(
+    "SELECT ativo FROM dgeo.usuario WHERE uuid = $<usuarioUuid>",
+    { usuarioUuid: decoded.uuid }
+  );
+  if (!response.ativo) {
+    throw new AppError("Usuário não está ativo", httpCode.Forbidden);
+  }
 
-module.exports = verifyLogin
+  req.usuarioUuid = decoded.uuid;
+  next();
+});
+
+module.exports = verifyLogin;
