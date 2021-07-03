7140976a5e00becdffa1aab8f3491c0f05d054c0
Felipe Diniz <diniz.ime@gmail.com>
2021-07-03 18:39:16 -0300

    min version

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ var version = process.versions.node.split('.')
 var major = +version[0]
 var minor = +version[1]
 
-if (major < 8 || (major === 8 && minor < 7)) {
+if (major < 14 || (major === 14 && minor < 15)) {
   throw new Error('Versão mínima do Node.js suportada pelo Serviço é 14.15')
 }
 
